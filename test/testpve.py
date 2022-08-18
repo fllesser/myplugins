497ca425099d4667be73c0e2b9afe501cc05e678
chowyijiu <chowyijiu@gmail.com>
2022-08-18 11:57:25 +0800

    忽略一些文件

--- test/testpve.py
+++
@@ -1,5 +0,0 @@
-import re
-sql = '\d dsfhsjkladjf; lksdjflkasdj flksdjfl;asdkjf klsdjflsdakfj ls;dkfj lsdkfj slkjdf lksdjf lkssdf sdfsdfsda fsda fsdfasdf'
-if re.search('select', sql, flags=0) or re.search(r'\\d', sql, flags=0):
-    print(sql[0:100])
-    print("hello")
