caa99a552a5d917c1ab7106a50e22fe811c5b3a1
chowyijiu <chowyijiu@gmail.com>
2022-08-18 12:19:50 +0800

    no 说明

--- test/test2.py
+++
@@ -1,7 +0,0 @@
-
-
-members = {"asddfs":123}
-
-if members.get("asddfss") is None:
-    members["asddfss"] = 3
-print(members)
--- test/testfnrank2.py
+++
@@ -1,6 +0,0 @@
-import imgkit
- 
-options = {
-    "encoding": "UTF-8"
-}
-imgkit.from_url('https://tracker.fortnitefrance.eu/stats/save', '/Users/yijiuchow/Desktop/helloworld.jpg', options=options)
