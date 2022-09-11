308139ca12b2b3c84eec116aef71c02ef404de7e
YiJiuChow <19550518zrf@gmail.com>
2022-09-11 19:28:52 +0800

    gm_increase 添加日志

--- nonebot_plugin_gm_manager/__init__.py
+++ nonebot_plugin_gm_manager/__init__.py
@@ -80,9 +80,10 @@ async def _(bot: Bot, event: GroupMessageEvent, arg: Message = CommandArg()):
 @gm_increase.handle()
 async def _(bot: Bot, event: GroupIncreaseNoticeEvent):
     group_info = await bot.get_group_info(group_id=event.group_id, no_cache=True)
+    logger.info(f"该群当前人数 {group_info['member_count']}")
     if group_info["member_count"] == group_info["max_member_count"]:
         await bot.send_group_msg(
-            message=f"检测到该群人数已满\n开始踢除不活跃用户\n当前规则:\n1.超过三个月不发言\n2.群活跃等级小于20",
+            message="检测到该群人数已满\n开始踢除不活跃用户\n当前规则:\n 1.超过三个月不发言\n 2.群活跃等级小于20",
             group_id=event.group_id)
         message_str = await kick_not_active_member(bot=bot,group_id=event.group_id,kicked_num=10)
         await gm_increase.finish(message=message_str)
