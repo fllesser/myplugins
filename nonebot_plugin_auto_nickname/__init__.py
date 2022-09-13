015108fc81affd28bdca563078c54b1c08652d8e
YiJiuChow <19550518zrf@gmail.com>
2022-09-14 00:40:28 +0800

    添加钩子函数

--- nonebot_plugin_auto_nickname/__init__.py
+++ nonebot_plugin_auto_nickname/__init__.py
@@ -1,5 +1,6 @@
-import time
+import time, asyncio
 from random import choice
+from nonebot import get_driver
 
 from services.log import logger
 from utils.utils import scheduler, get_bot
@@ -9,7 +10,17 @@ __zx_plugin_name__ = "时间群昵称"
 __plugin_version__ = 0.1
 __plugin_author__ = "YiJiuChow"
 
+
 lz_list = ["绿头鸡", "鸡脖哥", "鸡脖王", "鸡脖哥哥"]
+gl = []
+driver = get_driver()
+
+@driver.on_bot_connect
+async def init_gl():
+    bot = get_bot()
+    gl = await bot.call_api("get_group_list")
+    gl = [g["group_id"] for g in gl]
+    logger.info(f"群列表初始化成功 {gl}")
 
 @scheduler.scheduled_job(
     "interval",
@@ -18,11 +29,10 @@ lz_list = ["绿头鸡", "鸡脖哥", "鸡脖王", "鸡脖哥哥"]
 async def _():
     try:
         bot = get_bot()
-        gl = await bot.call_api("get_group_list")
-        gl = [g["group_id"] for g in gl]
         timestr = time.strftime("%a %b %d %H:%M %Y CST", time.localtime())
         for g in gl:
             await bot.set_group_card(group_id=g, user_id=bot.self_id, card=f"大头 {timestr}")
+            await asyncio.sleep(0.5)
         await bot.set_group_card(group_id=774331907, user_id=1626303708, card=choice(lz_list)) 
     except Exception as e:
         logger.error(f"error: {e}")
--- nonebot_plugin_gm_manager/model.py
+++ nonebot_plugin_gm_manager/model.py
@@ -4,7 +4,7 @@ from models.group_member_info import GroupInfoUser
 
 class GroupInfoUserByMe(GroupInfoUser):
 
-    query_start_dict = {"23213":1}
+    query_start_dict = {"1":1}
 
     @classmethod
     async def get_group_user_qq_list(cls, group_id: int, user_num: int, off_set: int) -> List[int]:
@@ -12,9 +12,9 @@ class GroupInfoUserByMe(GroupInfoUser):
         说明:
             获取该群指定数量用户qq
         参数:
-            :param group_id: 群号
-             user_num: 要获取的用户qq数量
-             off_set: 从哪行开始
+            param group_id: 群号
+            user_num: 要获取的用户qq数量
+            off_set: 从哪行开始
         """
         member_list = []
         query = cls.query.where((cls.group_id == group_id)).limit(user_num).offset(off_set - 1)
