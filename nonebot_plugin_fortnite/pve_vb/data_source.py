c93be2464eff0cf3486ebdf58f1ec7039f6bd6a5
YiJiuChow <19550518zrf@gmail.com>
2022-09-19 00:59:58 +0800

    战绩插件区分赛季和生涯战绩

--- nonebot_plugin_fortnite/pve_vb/data_source.py
+++ nonebot_plugin_fortnite/pve_vb/data_source.py
@@ -11,32 +11,34 @@ from services.log import logger
 from utils.image_utils import BuildImage
 from configs.path_config import IMAGE_PATH
 
+
 async def update_daily_vb():
     url = "https://freethevbucks.com/timed-missions/"
     html_content = httpx.get(url).content
     soup = BeautifulSoup(html_content, "lxml")
     # vb图
-    img = BuildImage(w=256, h=200, font_size=15, color=(36, 44, 68), font="gorga.otf")
+    img = BuildImage(w=256, h=200, font_size=15,
+                     color=(36, 44, 68), font="gorga.otf")
     Y = 30
     # 电力图标
-    ele_img = Image.open(BytesIO(httpx.get("https://img.icons8.com/office/30/000000/lightning-bolt.png").content))
+    ele_img = Image.open(BytesIO(httpx.get(
+        "https://img.icons8.com/office/30/000000/lightning-bolt.png").content))
     ele_img = ele_img.resize((20, 20), Image.LANCZOS)
     # 当前时间
-    timestr = time.strftime("%Y-%m-%d %H:%M:%S",time.localtime())
-    await img.atext(pos=(0, 170),text=timestr, center_type="by_width", fill=(255, 255, 255))
+    timestr = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    await img.atext(pos=(0, 170), text=timestr, center_type="by_width", fill=(255, 255, 255))
     for item in soup.find_all("p"):
-        if item.span is not None:
-            if item.span.b is not None:
-                storm_src = item.img.get("src") # 风暴图标链接
-                storm_img = httpx.get(storm_src).content
-                storm_img = Image.open(BytesIO(storm_img))
-                await img.apaste(img=storm_img, pos=(40, Y), alpha=True) # 风暴图标
-                await img.atext(text=item.b.string, pos=(70, Y-3), fill=(255, 255, 255)) # 电力
-                await img.apaste(img=ele_img, pos=(100, Y+1), alpha=True) # 电力图标
-                await img.atext(pos=(130, Y-3), text= item.span.text, fill=(255, 255, 255))
-                Y += 30
+        if item.span is not None and item.span.b is not None:
+            storm_src = item.img.get("src")  # 风暴图标链接
+            storm_img = httpx.get(storm_src).content
+            storm_img = Image.open(BytesIO(storm_img))
+            await img.apaste(img=storm_img, pos=(40, Y), alpha=True)  # 风暴图标
+            # 电力
+            await img.atext(text=item.b.string, pos=(70, Y-3), fill=(255, 255, 255))
+            await img.apaste(img=ele_img, pos=(100, Y+1), alpha=True)  # 电力图标
+            await img.atext(pos=(130, Y-3), text=item.span.text, fill=(255, 255, 255))
+            Y += 30
     if Y == 30:
         img.font = ImageFont.truetype(str(FONT_PATH / "HWXingKai.ttf"), 30)
-        await img.atext(text="今天没有vb图捏", pos=(0, 80), center_type="by_width", fill=(255, 255, 255))
+        await img.atext(pos=(0, 80), text="今天没有vb图捏", center_type="by_width", fill=(255, 255, 255))
     img.save(IMAGE_PATH / "fn_stw.png")
-    
--- nonebot_plugin_fortnite/rank/__init__.py
+++ nonebot_plugin_fortnite/rank/__init__.py
@@ -7,7 +7,7 @@ from utils.image_utils import pic2b64
 from configs.path_config import FONT_PATH
 from services.log import logger
 
-from fortnite_api import StatsImageType, FortniteAPI
+from fortnite_api import StatsImageType, FortniteAPI, TimeWindow
 from PIL import Image, ImageFont, ImageDraw
 from io import BytesIO
 import httpx, re
@@ -29,8 +29,8 @@ __plugin_des__ = "堡垒之夜战绩查询"
 
 api = FortniteAPI(api_key = "f3f4e682-346e-45b1-8323-fe77aaea2a68", run_async = True)
 
-fortniterank = on_command("战绩", block=True)
-@fortniterank.handle()
+season_stat = on_command("战绩", block=True)
+@season_stat.handle()
 async def _(bot: Bot, event: GroupMessageEvent, args: Message = CommandArg()):
     nickname = args.extract_plain_text()
     if nickname is None or nickname == '':
@@ -38,27 +38,14 @@ async def _(bot: Bot, event: GroupMessageEvent, args: Message = CommandArg()):
         if card is not None and card[0:3].casefold() in ["id:", "id：", "id ",]:
             nickname = card[3:len(card)] # 昵称替换为群名片id
         else:
-            await fortniterank.finish(message="群昵称(名片)设置为\n['id:', 'id：', 'id ']三选一(不区分大小写)\n加上你的游戏昵称发送 战绩 可快速查询战绩")
+            await season_stat.finish(message="群昵称(名片)设置为\n['id:', 'id：', 'id ']三选一(不区分大小写)\n加上你的游戏昵称发送 战绩 可快速查询当前赛季战绩")
     try:
-        playerstats = await api.stats.fetch_by_name(nickname,image=StatsImageType.ALL)
+        playerstats = await api.stats.fetch_by_name(nickname, time_window=TimeWindow.SEASON, image=StatsImageType.ALL)
         url = playerstats.image_url
         result = None
         # 匹配带中文昵称
         if re.search(r'[\u2E80-\u9FFF]', nickname, flags=0):
-            response = httpx.get(url)
-            im = Image.open(BytesIO(response.content))
-            draw = ImageDraw.Draw(im) 
-            # 覆盖
-            draw.rectangle(xy=(30, 90, 420, 230), fill="#012e57")
-            # 填充昵称
-            cn_len = (len(nickname.encode("utf-8")) - len(nickname.encode("GBK")))
-            nickname_len = (len(nickname) - cn_len + 0.0) / 2 + cn_len
-            font_size = 30
-            X = max((225 - nickname_len * font_size / 2), 0)
-            font = "HWXingKai.ttf"
-            ttfont = ImageFont.truetype(str(FONT_PATH / font), font_size)
-            draw.text((X, 150), f'{nickname}', fill = "#fafafa", font=ttfont)
-            result = im
+            result = write_chinese_nickname(url=url, nickname=nickname)
     except Exception as e:
         result = str(e)
         if "public" in result:
@@ -66,11 +53,61 @@ async def _(bot: Bot, event: GroupMessageEvent, args: Message = CommandArg()):
         elif "exist" in result:
             result = "用户不存在"
         elif "play any match" in result:
-            result = "该玩家最近没有进行过任何对局"
-        await fortniterank.finish(message=result)
+            result = "该玩家该赛季没有进行过任何对局"
+        await season_stat.finish(message=result)
     logger.info("战绩查询成功")
     if result is not None:
         result = image(b64=pic2b64(result))
     else:
         result = image(url)
-    await fortniterank.finish(message=result)
+    await season_stat.finish(message=result)
+
+lifetime_stat = on_command("生涯战绩", block=True)
+@lifetime_stat.handle()
+async def _(bot: Bot, event: GroupMessageEvent, args: Message = CommandArg()):
+    nickname = args.extract_plain_text()
+    if nickname is None or nickname == '':
+        card = (await bot.get_group_member_info(user_id=event.user_id, group_id=event.group_id, no_cache=True))["card"]
+        if card is not None and card[0:3].casefold() in ["id:", "id：", "id ",]:
+            nickname = card[3:len(card)] # 昵称替换为群名片id
+        else:
+            await lifetime_stat.finish(message="群昵称(名片)设置为\n['id:', 'id：', 'id ']三选一(不区分大小写)\n加上你的游戏昵称发送 生涯战绩 可快速查询生涯战绩")
+    try:
+        playerstats = await api.stats.fetch_by_name(nickname, image=StatsImageType.ALL)
+        url = playerstats.image_url
+        result = None
+        # 匹配带中文昵称
+        if re.search(r'[\u2E80-\u9FFF]', nickname, flags=0):
+            result = write_chinese_nickname(url=url, nickname=nickname)
+    except Exception as e:
+        result = str(e)
+        if "public" in result:
+            result = "战绩未公开"
+        elif "exist" in result:
+            result = "用户不存在"
+        elif "play any match" in result:
+            result = "该玩家没有进行过任何对局"
+        await lifetime_stat.finish(message=result)
+    logger.info("战绩查询成功")
+    if result is not None:
+        result = image(b64=pic2b64(result))
+    else:
+        result = image(url)
+    await lifetime_stat.finish(message=result)
+
+
+def write_chinese_nickname(url: str, nickname: str):
+    response = httpx.get(url)
+    im = Image.open(BytesIO(response.content))
+    draw = ImageDraw.Draw(im) 
+    # 覆盖
+    draw.rectangle(xy=(30, 90, 420, 230), fill="#012e57")
+    # 填充昵称
+    cn_len = (len(nickname.encode("utf-8")) - len(nickname.encode("GBK")))
+    nickname_len = (len(nickname) - cn_len + 0.0) / 2 + cn_len
+    font_size = 30
+    X = max((225 - nickname_len * font_size / 2), 0)
+    font = "HWXingKai.ttf"
+    ttfont = ImageFont.truetype(str(FONT_PATH / font), font_size)
+    draw.text((X, 150), f'{nickname}', fill = "#fafafa", font=ttfont)
+    return im
