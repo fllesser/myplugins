29d8d692e7c9278418720eedde59500b1a7ad512
YiJiuChow <19550518zrf@gmail.com>
2022-11-23 11:50:21 +0800

    修复过期不执行的bug

--- nonebot_plugin_todo/__init__.py
+++ nonebot_plugin_todo/__init__.py
@@ -24,14 +24,16 @@ async def _(event: PrivateMessageEvent, args: Message = CommandArg()):
     job_str = args.extract_plain_text().split(' ')
     todo_dict[job_str[0]] = job_str[1]
     lt = time.localtime()
-    scheduler.add_job(
-        todo_aps, "date", 
-        run_date = datetime.datetime(lt.tm_year, lt.tm_mon, lt.tm_mday, int(job_str[0][0:2]), int(job_str[0][2:4]), 0),
-        id = job_str[0], args = [job_str[0]])
-    await todo.finish(message=MyStr()
+    job_hour = job_str[0][0:2]
+    job_minute = job_str[0][2:4]
+    run_date = datetime.datetime(lt.tm_year, lt.tm_mon, lt.tm_mday, int(job_hour), int(job_minute), 0)
+    if lt.tm_hour > int(job_hour) or (lt.tm_hour == int(job_hour) and lt.tm_min >= job_minute):
+        run_date = run_date + datetime.timedelta(days=1)
+    scheduler.add_job(todo_aps, "date", run_date = run_date, id = job_str[0], args = [job_str[0]])
+    await todo.finish(message = MyStr()
             .append_line("TODO JOB ADD SUCCESS")
             .append_line("  You're going to do:")
-            .append_line(f"  - {job_str[0][0:2]}:{job_str[0][2:4]} {job_str[1]}")
+            .append_line(f"  - {job_hour}:{job_minute} {job_str[1]}")
             .append_line("  I'll remind you then.")
             .append("END")
         )
@@ -42,8 +44,7 @@ async def _():
     td_list = MyStr()
     for k, v in todo_dict.items():
         td_list.append_line(f"  - {k[0:2]}:{k[2:4]} {v}")
-    await todo_list.finish(message=
-        MyStr()
+    await todo_list.finish(message = MyStr()
             .append_line("TODO JOB LIST")
             .append_line("")
             .append_line(td_list.value)
@@ -52,7 +53,7 @@ async def _():
 async def todo_aps(job_id: str):
     bot = get_bot()
     await bot.send_private_msg(user_id='1942422015', 
-        message=MyStr()
+        message = MyStr()
             .append_line("TODO JOB")
             .append_line("  You should be right now:")
             .append_line(f"  - {todo_dict[job_id]}")
