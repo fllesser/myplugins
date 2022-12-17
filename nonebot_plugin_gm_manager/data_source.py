12e7b91c96e8b1dfd5603912f0249d2310598494
elastichow <19550518zrf@gmail.com>
2022-12-17 21:09:36 +0800

    ...

--- nonebot_plugin_gm_manager/data_source.py
+++ nonebot_plugin_gm_manager/data_source.py
@@ -43,7 +43,9 @@ async def get_kicked_list(bot: Bot, group_id: int, kicked_num: int) -> List[Dict
             except:
                 await GroupInfoUserByMe.delete_member_info(user_qq=member_qq, group_id=group_id)
                 continue
-            if (now_time - int(member["last_sent_time"]) > 7777777) and int(member["level"]) <= 1 and ((member["title"] is None or member["title"] == '')):
+            if ((now_time - int(member["last_sent_time"]) > 7777777) and # 三个月未发言
+                int(member["level"]) <= 1 and # 群活跃等级小于等于20
+                (member["title"] is None or member["title"] == '')): # 群昵称为空
                 members.append(member)
             if len(members) == kicked_num:
                 return members
