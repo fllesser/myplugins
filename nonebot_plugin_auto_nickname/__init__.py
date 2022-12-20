76d7fe95bd506e05fbc6e39516cb34521cf69b03
elastichow <19550518zrf@gmail.com>
2022-12-20 14:26:28 +0800

    添加新群

--- nonebot_plugin_auto_nickname/__init__.py
+++ nonebot_plugin_auto_nickname/__init__.py
@@ -10,7 +10,7 @@ __plugin_version__ = 0.1
 __plugin_author__ = "YiJiuChow"
 
 
-lz_list = ["绿头鸡", "鸡脖哥", "鸡脖王", "鸡脖哥哥"]
+# lz_list = ["绿头鸡", "鸡脖哥", "鸡脖王", "鸡脖哥哥"]
 gl = [754044548, 208248400] # [669026253,, 782270111, 1080197262, 1149277515, 913941037] # 
 # bot = get_bot()
 # gl = bot.call_api("get_group_list")
@@ -18,22 +18,22 @@ gl = [754044548, 208248400] # [669026253,, 782270111, 1080197262, 1149277515, 91
 # bot = None
 # logger.info(f"群列表初始化成功 {gl}")
 
+bot = get_bot()
+
 @scheduler.scheduled_job("interval", minutes=1)
 async def _():
     try:
-        bot = get_bot()
         timestr = time.strftime("%a %b %d %H:%M %Y CST", time.localtime())
         for g in gl:
             await bot.set_group_card(group_id=g, user_id=bot.self_id, card=f"大头 {timestr}")
             await asyncio.sleep(1)
-        await bot.set_group_card(group_id=774331907, user_id=1626303708, card=choice(lz_list)) 
+        # await bot.set_group_card(group_id=774331907, user_id=1626303708, card=choice(lz_list)) 
     except Exception as e:
         logger.error(f"error: {e}")
 
 @scheduler.scheduled_job("cron", hour=6, minute=0,)
 async def _():
     try:
-        bot = get_bot()
         timestr = time.strftime("%a %b %d %Y CST", time.localtime())
         await bot.call_api("set_qq_profile", nickname=f"大头 {timestr}")
     except Exception as e:
