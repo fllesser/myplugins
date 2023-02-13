aaede8d113dc21557c054c86aeaf085e808ef608
elastichow <19550518zrf@gmail.com>
2023-02-13 20:57:59 +0800

    group card notice event

--- nonebot_plugin_gm_manager/class_.py
+++ nonebot_plugin_gm_manager/class_.py
@@ -3,6 +3,9 @@ from nonebot.adapters.onebot.v11 import (
 )
 from typing import Literal
 
+from nonebot.typing import overrides
+
+## todo 不能这样写捏
 class GroupCardNoticeEvent(NoticeEvent):
     """群名片修改事件"""
 
@@ -11,3 +14,11 @@ class GroupCardNoticeEvent(NoticeEvent):
     group_id: int
     card_old: str
     card_new: str
+
+    @overrides(NoticeEvent)
+    def get_user_id(self) -> str:
+        return str(self.user_id)
+
+    @overrides(NoticeEvent)
+    def get_session_id(self) -> str:
+        return f"group_{self.group_id}_{self.user_id}"
