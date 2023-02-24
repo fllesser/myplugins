cc2ac53a42bf8efa822d85106c3d3e11dc932508
elastichow <19550518zrf@gmail.com>
2023-02-24 16:53:03 +0800

    群昵称修改,添加qq号显示

--- nonebot_plugin_gm_manager/__init__.py
+++ nonebot_plugin_gm_manager/__init__.py
@@ -91,8 +91,9 @@ async def _(bot: Bot, event: NoticeEvent):
             return
         new_card = event.card_new
         if new_card[0:3].casefold() in ["id:", "id：", "id ",]:
-            new_card += "(昵称符合查询战绩/季卡规则)"
-        await bot.send_group_msg(group_id=event.group_id, message=event.card_old + " 修改群昵称为 " + new_card)
+            new_card += "(新昵称符合查询战绩/季卡条件)"
+        await bot.send_group_msg(group_id=event.group_id, 
+                                 message=f"{event.card_old}({event.get_user_id})修改群昵称为{new_card}")
 
 # 手动命令
 banuser = on_command("ban", priority=5, block=True)
