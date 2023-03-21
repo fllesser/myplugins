1485eb36548d8c5d00cc1a4ec00dd64f7bc6b96b
elastichow <19550518zrf@gmail.com>
2023-03-21 12:14:54 +0800

    gm manager 更新适配

--- nonebot_plugin_gm_manager/model.py
+++ nonebot_plugin_gm_manager/model.py
@@ -16,8 +16,34 @@ class GroupInfoUserByMe(GroupInfoUser):
             user_num: 要获取的用户qq数量
             off_set: 从哪行开始
         """
+        return list(
+            await cls.filter(group_id=group_id).values_list("user_qq", flat=True).limit(user_num).offset(off_set - 1)
+        )
+    
+    @classmethod
+    async def delete_member_info(cls, user_qq: int, group_id: int) -> bool:
+        """
+        说明:
+            删除群员信息
+        参数:
+            :param user_qq: qq号
+            :param group_id: 群号
+        """
+        await cls.filter(user_qq=user_qq, group_id=group_id).delete()
+
+    @classmethod
+    async def get_group_user_qq_list2(cls, group_id: int, user_num: int, off_set: int) -> List[int]:
+        """
+        说明:
+            获取该群指定数量用户qq
+        参数:
+            param group_id: 群号
+            user_num: 要获取的用户qq数量
+            off_set: 从哪行开始
+        """
         member_list = []
         query = cls.query.where((cls.group_id == group_id)).limit(user_num).offset(off_set - 1)
         for user in await query.gino.all():
             member_list.append(user.user_qq)
         return member_list
+    
