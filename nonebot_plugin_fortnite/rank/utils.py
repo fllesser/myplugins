121fd709d65ec490b5bf3b053e7def6ac8854e96
elastichow <19550518zrf@gmail.com>
2023-03-21 14:20:47 +0800

    image.size

--- nonebot_plugin_fortnite/rank/utils.py
+++ nonebot_plugin_fortnite/rank/utils.py
@@ -24,8 +24,8 @@ def _init_rank_graph(
             f"{IMAGE_PATH}/background/create_mat/{x}"
             for x in os.listdir(f"{IMAGE_PATH}/background/create_mat")
         ],
-        font_size=int(12 * (1 - len(_uname_lst) / 100)),
         bar_color=["*"],
     )
+    image.size = (1200, 1000)
     image.gen_graph()
     return image
