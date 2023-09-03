0880c1ed193d52904adb77d3f784ac144b17654d
elastichow <19550518zrf@gmail.com>
2023-09-03 10:21:28 +0800

    group_id

--- nonebot_plugin_auto_nickname/__init__.py
+++ nonebot_plugin_auto_nickname/__init__.py
@@ -13,12 +13,12 @@ __plugin_author__ = "YiJiuChow"
 # lz_list = ["绿头鸡", "鸡脖哥", "鸡脖王", "鸡脖哥哥"]
 # gl = [754044548, 208248400] # [669026253,, 782270111, 1080197262, 1149277515, 913941037] # 
 
-gl: list
+gl: list = []
 
 @scheduler.scheduled_job("interval", minutes=1)
 async def _():
     bot = get_bot()
-    if not gl:
+    if len(gl) == 0:
         gl = [g["group_id"] for g in bot.get_group_list()]
     if bot:
         timestr = time.strftime("%a %b %d %H:%M %Y CST", time.localtime())
